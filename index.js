658025d1e2884413c487f05612718415d0b47e45
IzePhanthakarn <izephanthakarn@hotmail.com>
2023-09-28 00:42:29 +0700

    🎉 Begin a project

---
+++ index.js
@@ -0,0 +1,79 @@
+require("dotenv/config");
+const { Client } = require("discord.js");
+const { OpenAI } = require("openai");
+
+const client = new Client({
+  intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
+});
+
+const IGNORE_PREFIX = "!";
+const CHANNELS = [process.env.CHANNEL_ID];
+
+let onlineNotificationChannel;
+
+client.on("ready", () => {
+  console.log("The bot is online.");
+
+  onlineNotificationChannel = client.channels.cache.get(CHANNELS[0]);
+
+  if (onlineNotificationChannel) {
+    onlineNotificationChannel.send("🌟 The bot is now online! 🟢 #StayConnected");
+  }
+
+});
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY,
+});
+
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return;
+  if (message.content.startsWith(IGNORE_PREFIX)) return;
+  if (
+    !CHANNELS.includes(message.channelId) &&
+    !message.mentions.users.has(client.user.id)
+  )
+    return;
+
+  await message.channel.sendTyping();
+
+  const sendTypingInterval = setInterval(() => {
+    message.channel.sendTyping();
+  }, 5000);
+
+  const response = await openai.chat.completions
+    .create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          // name:
+          role: "system",
+          content: "Chat GPT is a friendly chatbot.",
+        },
+        {
+          // name:
+          role: "user",
+          content: message.content,
+        },
+      ],
+    })
+    .catch((error) => console.error("OpenAI Error:\n", error));
+
+  clearInterval(sendTypingInterval);
+
+  if (!response) {
+    message.reply(
+      "I'm having some trouble with the OpenAI API. Try again in a moment."
+    );
+    return;
+  }
+  const responseMessage = response.choices[0].message.content;
+  const chunkSizeLimit = 2000;
+
+  for (let i = 0; i < responseMessage.length; i += chunkSizeLimit) {
+    const chunk = responseMessage.substring(i, i + chunkSizeLimit);
+    await message.reply(chunk);
+  }
+});
+
+client.login(process.env.TOKEN);
