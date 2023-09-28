b198807293d4cb0f724f8a2cc73e4887ecae7545
IzePhanthakarn <izephanthakarn@hotmail.com>
2023-09-28 10:02:17 +0700

    ♻️ Refactor code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+// Packages used in the project
 require("dotenv/config");
 const { Client } = require("discord.js");
 const { OpenAI } = require("openai");
 
+// Create Instance of Discord Client
 const client = new Client({
   intents: ["Guilds", "GuildMembers", "GuildMessages", "MessageContent"],
 });
@@ -9,40 +11,46 @@ const client = new Client({
 const IGNORE_PREFIX = "!";
 const CHANNELS = [process.env.CHANNEL_ID];
 
+// Bot connects successfully to Discord
 client.on("ready", () => {
   console.log("The bot is online.");
 });
 
+// Create Instance of OpenAI
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_KEY,
 });
 
+// A new message has arrived in the Discord chat
 client.on("messageCreate", async (message) => {
+  // The message sender is a bot
   if (message.author.bot) return;
+  // The message is start with IGNORE_PREFIX
   if (message.content.startsWith(IGNORE_PREFIX)) return;
+  // Check channel and mentions to bot
   if (
     !CHANNELS.includes(message.channelId) &&
     !message.mentions.users.has(client.user.id)
   )
     return;
 
+  // The bot will display the typing in the chat box
   await message.channel.sendTyping();
 
   const sendTypingInterval = setInterval(() => {
     message.channel.sendTyping();
   }, 5000);
 
+  // Forward user-sent messages to OpenAI
   const response = await openai.chat.completions
     .create({
-      model: "gpt-3.5-turbo",
+      model: "gpt-3.5-turbo",   // model of chat bot
       messages: [
         {
-          // name:
           role: "system",
           content: "Chat GPT is a friendly chatbot.",
         },
         {
-          // name:
           role: "user",
           content: message.content,
         },
@@ -52,12 +60,15 @@ client.on("messageCreate", async (message) => {
 
   clearInterval(sendTypingInterval);
 
+  // Check response
   if (!response) {
     message.reply(
       "I'm having some trouble with the OpenAI API. Try again in a moment."
     );
     return;
   }
+
+  // Divide text into chunks of up to 2000 characters
   const responseMessage = response.choices[0].message.content;
   const chunkSizeLimit = 2000;
 
@@ -67,4 +78,5 @@ client.on("messageCreate", async (message) => {
   }
 });
 
+// Logging into Discord
 client.login(process.env.TOKEN);
